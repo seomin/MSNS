e456983d7988938359e3f7badb9826ccb5771e4e
mushruptor2 <s2671781@mail.zih.tu-dresden.de>
2014-11-11 15:29:42 +0100

    Bootstrap css

--- OldSWP14/NetSpecAsPlay/Netspec/app/assets/javascripts/Netapp.js
+++ OldSWP14/NetSpecAsPlay/Netspec/app/assets/javascripts/Netapp.js
@@ -1,96 +1,94 @@
-require(["webjars!knockout.js", 'webjars!jquery.js', "/routes.js", "webjars!bootstrap.js"], (ko) ->
+require(["webjars!knockout.js", 'webjars!jquery.js', "/routes.js", "webjars!bootstrap.js"], function(ko) {
 
-  messagesPerPage = 10
 
-  # Models for the messages page
-  class MessagesModel
-    constructor: () ->
-      self = @
-      # the list of messages
-      @messages = ko.observableArray()
-
-      # the messages field that messages are entered into
-      @messageField = ko.observable()
-
-      # the URL to fetch the next page of messages, if one exists
-      @nextMessagesUrl = ko.observable()
-
-      # the URL to fetch the previous page of messages, if one exists
-      @prevMessagesUrl = ko.observable()
-
-      # save a new message
-      @saveMessage = () ->
-        @ajax(routes.controllers.MessageController.saveMessage(), {
+  var MessagesModel, events, messagesPerPage, model;
+  messagesPerPage = 10;
+  MessagesModel = (function() {
+    function MessagesModel() {
+      var self;
+      self = this;
+      this.messages = ko.observableArray();
+      this.messageField = ko.observable();
+      this.nextMessagesUrl = ko.observable();
+      this.prevMessagesUrl = ko.observable();
+      this.saveMessage = function() {
+        return this.ajax(routes.controllers.MessageController.saveMessage(), {
           data: JSON.stringify({
-            message: @messageField()
-          })
+            message: this.messageField()
+          }),
           contentType: "application/json"
-        }).done(() ->
-          $("#addMessageModal").modal("hide")
-          self.messageField(null)
-        )
+        }).done(function() {
+          $("#addMessageModal").modal("hide");
+          return self.messageField(null);
+        });
+      };
+      this.getMessages = function() {
+        return this.ajax(routes.controllers.MessageController.getMessages(0, messagesPerPage)).done(function(data, status, xhr) {
+          return self.loadMessages(data, status, xhr);
+        });
+      };
+      this.nextMessages = function() {
+        if (this.nextMessagesUrl()) {
+          return $.ajax({
+            url: this.nextMessagesUrl()
+          }).done(function(data, status, xhr) {
+            return self.loadMessages(data, status, xhr);
+          });
+        }
+      };
+      this.prevMessages = function() {
+        if (this.prevMessagesUrl()) {
+          return $.ajax({
+            url: this.prevMessagesUrl()
+          }).done(function(data, status, xhr) {
+            return self.loadMessages(data, status, xhr);
+          });
+        }
+      };
+    }
 
-      # get the messages
-      @getMessages = () ->
-        @ajax(routes.controllers.MessageController.getMessages(0, messagesPerPage))
-          .done((data, status, xhr) ->
-            self.loadMessages(data, status, xhr)
-          )
+    MessagesModel.prototype.ajax = function(route, params) {
+      return $.ajax($.extend(params, route));
+    };
 
-      # get the next page of messages
-      @nextMessages = () ->
-        if @nextMessagesUrl()
-          $.ajax({url: @nextMessagesUrl()}).done((data, status, xhr) ->
-            self.loadMessages(data, status, xhr)
-          )
+    MessagesModel.prototype.loadMessages = function(data, status, xhr) {
+      var link, next, prev;
+      this.messages(data);
+      link = xhr.getResponseHeader("Link");
+      if (link) {
+        next = /.*<([^>]*)>; rel="next".*/.exec(link);
+        if (next) {
+          this.nextMessagesUrl(next[1]);
+        } else {
+          this.nextMessagesUrl(null);
+        }
+        prev = /.*<([^>]*)>; rel="prev".*/.exec(link);
+        if (prev) {
+          return this.prevMessagesUrl(prev[1]);
+        } else {
+          return this.prevMessagesUrl(null);
+        }
+      } else {
+        this.nextMessagesUrl(null);
+        return this.prevMessagesUrl(null);
+      }
+    };
 
-      # get the previous page of messages
-      @prevMessages = () ->
-        if @prevMessagesUrl()
-          $.ajax({url: @prevMessagesUrl()}).done((data, status, xhr) ->
-            self.loadMessages(data, status, xhr)
-          )
+    return MessagesModel;
 
-    # Convenience ajax request function
-    ajax: (route, params) ->
-      $.ajax($.extend(params, route))
-
-    # Handle the messages response
-    loadMessages: (data, status, xhr) ->
-      @messages(data)
-
-      # Link handling for paging
-      link = xhr.getResponseHeader("Link")
-      if link
-        next = /.*<([^>]*)>; rel="next".*/.exec(link)
-        if next
-          @nextMessagesUrl(next[1])
-        else
-          @nextMessagesUrl(null)
-        prev = /.*<([^>]*)>; rel="prev".*/.exec(link)
-        if prev
-          @prevMessagesUrl(prev[1])
-        else
-          @prevMessagesUrl(null)
-      else
-        @nextMessagesUrl(null)
-        @prevMessagesUrl(null)
-
-  # Setup
-  model = new MessagesModel
-  ko.applyBindings(model)
-  # Load messages data
-  model.getMessages()
-
-  # Server Sent Events handling
-  events = new EventSource(routes.controllers.MainController.events().url)
-  events.addEventListener("message", (e) ->
-    # Only add the data to the list if we're on the first page
-    if model.prevMessagesUrl() == null
-      message = JSON.parse(e.data)
-      model.messages.unshift(message)
-      # Keep messages per page limit
-      if model.messages().length > messagesPerPage
-        model.messages.pop()
-  , false)
-)
+  })();
+  model = new MessagesModel;
+  ko.applyBindings(model);
+  model.getMessages();
+  events = new EventSource(routes.controllers.MainController.events().url);
+  return events.addEventListener("message", function(e) {
+    var message;
+    if (model.prevMessagesUrl() === null) {
+      message = JSON.parse(e.data);
+      model.messages.unshift(message);
+      if (model.messages().length > messagesPerPage) {
+        return model.messages.pop();
+      }
+    }
+  }, false);
+});
